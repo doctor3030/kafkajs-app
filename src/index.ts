4479d5433c0239775df570ccc386e2be4ec4914b
doctor3030 <dimamano@gmail.com>
2022-02-10 20:30:25 -0500

    fixed export

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as Connector from './kafka_app'
+import * as App from './kafka_app'
+
+declare namespace kafkajsConnector {
+  // Hoisted namespaces from other modules
+  export import connector = Connector;
+  export import app = App;
+}
+
+export = kafkajsConnector;
