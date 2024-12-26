ceaeac69287bd7c8f744ebdffb8c865a65f29972
doctor3030 <dimamano@gmail.com>
2024-12-25 19:31:14 -0500

    v0.2.1 quick fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as Connector from './kafka_connector'
 import * as App from './kafka_app'
+import * as AppPipelines from './kafka_app_pipeline'
 
 export import connector = Connector;
 export import app = App;
+export import appPipelines = AppPipelines;
